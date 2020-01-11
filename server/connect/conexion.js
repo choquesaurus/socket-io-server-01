c4719f3012aa5fe19c2308a60ef67ad60debb6c2
Wasauskyok <daniechoque159@gmail.com>
2020-01-11 06:05:49 -0500

    add  variables de entorno

--- server/connect/conexion.js
+++ server/connect/conexion.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
-import {cadena} from '../configuration/config';
+require('dotenv').config();
+//import {cadena} from '../configuration/config';
 //const cadena="mongodb+srv://gouser2:1234@cluster0-bzauq.mongodb.net/ChatExample01?retryWrites=true&w=majority&authSource=admin";
-mongoose.connect(cadena,{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false})
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false})
 .then(bd=>{
     console.log('db is connected');
 })
